c78811580dd31ae366a58cce9170469156c9c5aa
Shukajnhon <87706728+Shukajnhon@users.noreply.github.com>
2022-10-25 16:05:01 +0700

    add request Targeting all Articles, resquest targeting a specific article

--- app.js
+++ app.js
@@ -20,15 +20,102 @@ const articleSchema = mongoose.Schema({
 
 const Article = mongoose.model("Article", articleSchema)
 
-app.get("/articles", (req, res) => {
-    Article.find({}, (err, foundArticles) => {
-        if (!err) {
-            res.send(foundArticles)
-        } else {
-            res.send(err)
-        }
+////////////////////// Request Targeting all Articles///////////////
+
+app.route("/articles")
+    .get((req, res) => {
+        Article.find({}, (err, foundArticles) => {
+            if (!err) {
+                res.send(foundArticles)
+            } else {
+                res.send("No articles matching that title was found.")
+            }
+        })
     })
-})
+
+    .post((req, res) => {
+        const title = req.body.title
+        const content = req.body.content
+        console.log(title, content)
+
+        const newArticle = new Article({
+            title: title,
+            content: content
+        })
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send("Successfully added a new article.")
+            } else {
+                res.send(err)
+            }
+        })
+
+    })
+
+    .delete((req, res) => {
+        // Delete all
+        Article.deleteMany((err) => {
+            if (!err) {
+                res.send("Successfully delete all articles.")
+            } else {
+                res.send(err)
+            }
+        })
+    });
+
+
+////////////////////// Request Targeting A Specific Article ///////////////
+
+app.route("/articles/:articleTitle")
+    .get((req, res) => {
+        const articleTitle = req.params.articleTitle
+        Article.findOne({ title: articleTitle }, (err, foundArticle) => {
+            if (foundArticle) {
+                res.send(foundArticle)
+            } else {
+                res.send("No articles matching that title was found.")
+            };
+        })
+    })
+
+    .put((req, res) => {
+        Article.updateOne(
+            { title: req.params.articleTitle }, // condition
+            {
+                $set: {
+                    title: req.body.title,      // Update Title
+                    content: req.body.content   // Update Content
+                }
+            },
+            { overwrite: true },
+            function (err) {
+                if (!err) {
+                    res.send("Successfully update article.")
+                } else {
+                    res.send(err)
+                }
+            }
+
+        )
+    })
+
+    .delete((req, res) => {
+
+        Article.deleteOne(
+            {
+                title: req.params.articleTitle
+            },
+            function (err) {
+                if (!err) {
+                    res.send("Successfully deleted a article.")
+                } else {
+                    res.send(err)
+                }
+            }
+        )
+    });
+
+
 
 
 
